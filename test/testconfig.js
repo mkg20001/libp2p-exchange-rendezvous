f86fae5deb7ded10d302ced27649a912b21818b1
Maciej Krüger <mkg20001@gmail.com>
2018-06-17 10:44:18 +0200

    misc: move testconfig into it's own file

---
+++ test/testconfig.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const {parallel} = require('async')
+
+module.exports = {
+  opt: {
+    peerA: {
+      addrs: []
+    },
+    peerB: {
+      addrs: []
+    },
+    peerM: {
+      addrs: ['/ip4/127.0.0.1/tcp/5394/ws']
+    },
+    exchangeM: {
+      enableServer: true
+    }
+  },
+  before: (eA, eB, eM, cb) => {
+    parallel([eA, eB].map(e => cb => e.swarm.dial(eM.swarm.peerInfo, cb)), err => {
+      if (err) {
+        return cb(err)
+      }
+
+      setTimeout(() => cb(), 250) // wait for peers to find server
+    })
+  },
+  Exchange: require('../src')
+}
