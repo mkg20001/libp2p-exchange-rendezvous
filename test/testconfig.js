0194440d7da93c43c3647667d1309ba77d7a7cba
guysv <sviryguy@gmail.com>
2019-06-21 22:32:46 +0300

    Remove Some Bloat

--- test/testconfig.js
+++ test/testconfig.js
@@ -2,45 +2,35 @@
 
 const {parallel} = require('async')
 
-module.exports = (secure) => {
-  var Exchange = require('../src')
-  if (!secure) {
-    Exchange = new Proxy(Exchange, {
-      construct (ExchangeClass, [swarm, options]) {
-        if (!options) {
-          options = {}
-        }
-        options.secure = false
-        return new ExchangeClass(swarm, options)
+module.exports = (secure) => ({
+  opt: {
+    peerA: {
+      addrs: []
+    },
+    peerB: {
+      addrs: []
+    },
+    peerM: {
+      addrs: ['/ip4/127.0.0.1/tcp/5394/ws']
+    },
+    exchangeA: {
+      secure
+    },
+    exchangeB: {
+      secure
+    },
+    exchangeM: {
+      enableServer: true
+    }
+  },
+  before: (eA, eB, eM, cb) => {
+    parallel([eA, eB].map(e => cb => e.swarm.dial(eM.swarm.peerInfo, cb)), err => {
+      if (err) {
+        return cb(err)
       }
+
+      setTimeout(() => cb(), 250) // wait for peers to find server
     })
-  }
-
-
-  return {
-    opt: {
-      peerA: {
-        addrs: []
-      },
-      peerB: {
-        addrs: []
-      },
-      peerM: {
-        addrs: ['/ip4/127.0.0.1/tcp/5394/ws']
-      },
-      exchangeM: {
-        enableServer: true
-      }
-    },
-    before: (eA, eB, eM, cb) => {
-      parallel([eA, eB].map(e => cb => e.swarm.dial(eM.swarm.peerInfo, cb)), err => {
-        if (err) {
-          return cb(err)
-        }
-
-        setTimeout(() => cb(), 250) // wait for peers to find server
-      })
-    },
-    Exchange
-  }
-}
+  },
+  Exchange: require('../src')
+})
